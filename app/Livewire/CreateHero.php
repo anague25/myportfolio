50a381a9792c5fc450fe6cb62dbc823b28fa2de0
Anague Sonna Roosvelt <anaguesonnaroosvelt@gmail.com>
2023-12-05 21:44:29 +0100

    my portfolio with laravel, livewire and boostrap

---
+++ app/Livewire/CreateHero.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class CreateHero extends Component
+{
+    public function render()
+    {
+        return view('livewire.create-hero');
+    }
+}
